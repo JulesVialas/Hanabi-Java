272a24b9f6008c6dc4ae67b3732820e56202ca74
Jules <68593918+JulesVialas@users.noreply.github.com>
2024-05-17 11:18:10 +0200

    modifications toString et JAVADOC

--- src/jeu/Carte.java
+++ src/jeu/Carte.java
@@ -1,32 +1,29 @@
 /*
  * Carte.java			11/05/2024
- * IUT de Rodez, pas de copyrights
+ * IUT de Rodez, pas de copyright
  */
 
 package jeu;
 
 /**
  * La classe Carte représente une carte du jeu Hanabi. Chaque carte possède une
- * couleur et un numéro.
+ * couleur et une hauteur. Le jeu hanabi comporte .... 
  */
 public class Carte {
     /** La couleur de la carte. */
     private String couleur;
 
-    /** Le numéro de la carte. */
+    /** La hauteur de la carte. */
     private int hauteur;
 
-    /** Indique si le numéro est connu ou non. */
+    /** Indique si la hauteur est connu ou non. */
     private boolean hauteurConnue;
 
-    /** Indique si la carte est connue ou non. */
+    /** Indique si la couleur est connue ou non. */
     private boolean couleurConnue;
-
+    
     /**
-     * Constructeur de la classe Carte.
-     *
-     * @param couleur La couleur de la carte.
-     * @param numero  Le numéro de la carte.
+     * La hauteur et la couleur connue sont définis sur faux.
      */
     public Carte(String couleur, int hauteur) {
 	this.couleur = couleur;
@@ -36,43 +33,36 @@ public class Carte {
     }
 
     /**
-     * Retourne la couleur de la carte.
-     *
-     * @return La couleur de la carte.
+     * @return La couleur de la carte
      */
     public String getCouleur() {
 	return couleur;
     }
 
     /**
-     * Retourne le numéro de la carte.
-     *
-     * @return Le numéro de la carte.
+     * @return La hauteur de la carte
      */
     public int getHauteur() {
 	return hauteur;
     }
 
     /**
-     * Retourne si la couleur est visible ou non.
-     *
-     * @return le status de la visibilité de la carte .
+     * @return le status de la visibilité de la couleur de la carte
      */
     public boolean getCouleurConnue() {
 	return couleurConnue;
     }
 
     /**
-     * Retourne si la hauteur est visible ou non.
-     *
-     * @return le status de la visibilité de la carte .
+     * @return le status de la visibilité de la hauteur de la carte
      */
     public boolean getHauteurConnue() {
 	return hauteurConnue;
     }
 
     /**
-     * Défini si la couleur est visible ou non. * @param etat de la couleur.
+     * Défini si la couleur est visible ou non.
+     * @param etat de la visibilité de la couleur. True pour couleur connue sinon false
      */
     public void setCouleurConnue(boolean etat) {
 	couleurConnue = etat;
@@ -81,20 +71,19 @@ public class Carte {
     /**
      * Défini si la hauteur est visible ou non.
      *
-     * @param etat de la hauteur.
+     * @param etat de la visibilité de la hauteur. True pour hauteur connue sinon false
      */
     public void setHauteurConnue(boolean etat) {
 	hauteurConnue = etat;
     }
 
     /**
-     * Renvoie une représentation textuelle de la carte.
-     *
+     * Renvoie la couleur et la hauteur de la carte ainsi que l'état de la visiblité de ces deux champs.
      * @return Une chaîne de caractères représentant la carte au format [Couleur
      *         Numéro].
      */
     @Override
     public String toString() {
-	return "[" + couleur + " " + hauteur + "]";
+	return "[" + getCouleur() + " " + getHauteur() + " couleur visible " + getCouleurConnue() + " hauteur visible " + gethauteurConnue() "]";
     }
 }
