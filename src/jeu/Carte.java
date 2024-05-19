26f59aaec238504710728b3ee3c8da7c842af03f
Corentin Sol <corentin.solignac@iut-rodez.fr>
2024-05-19 09:07:27 +0200

    refactor: enum de Couleur

--- src/jeu/Carte.java
+++ src/jeu/Carte.java
@@ -1,13 +1,13 @@
 /*
  * Carte.java			11/05/2024
- * IUT de Rodez, pas de copyright
+ * IUT de Rodez, TP42, pas de copyright
  */
 
 package jeu;
 
 /**
- * La classe Carte représente une carte du jeu Hanabi. Chaque carte possède une
- * couleur et une valeur. 
+ * La classe Carte représente une carte du jeu Hanabi. 
+ * Chaque carte possède une couleur et une valeur. 
  * Le jeu Hanabi comporte : 
  * <ul>
  * 	<li>50 cartes</li>
@@ -15,27 +15,38 @@ package jeu;
  * 	<li>3 jetons rouges</li>
  * </ul>
  * <br>
- *  Pour chacune des 5 couleurs des cartes,  la distribution des valeurs est 
- *  1, 1, 1, 2, 2, 3, 3, 4, 4, 5.
+ *  Pour chacune des 5 couleurs des cartes,  la distribution des  
+ *  valeurs est 1, 1, 1, 2, 2, 3, 3, 4, 4, 5.
  */
 public class Carte {
     
-    /** La couleur de la carte. */
-    private String couleur;
+    /**
+     * Défini les couleurs de cartes valides pour le jeu Hanabi
+     */
+    public enum Couleur {
+        ROUGE, JAUNE, VERT, BLEU, BLANC
+    };
+    
+    /** La couleur de la carte */
+    private Couleur couleur;
 
-    /** La valeur de la carte. */
+    /** La valeur de la carte */
     private int valeur;
 
-    /** Indique si la couleur est connue ou non. */
+    /** Indique si la couleur est connue ou non par le joueur qui l'a */
     private boolean couleurConnue;
     
-    /** Indique si la valeur est connu ou non. */
+    /** Indique si la valeur est connu ou non par le joueur qui l'a */
     private boolean valeurConnue;
     
     /**
-     * La valeur et la couleur connue sont définis sur faux.
+     * La valeur et la couleur connue sont définis sur faux par 
+     * défaut
+     * 
+     * @param couleur la couleur de la carte
+     * @param valeur la valeur de la carte
      */
-    public Carte(String couleur, int valeur) {
+    public Carte(Couleur couleur, int valeur) {
 	this.couleur = couleur;
 	this.valeur = valeur;
 	this.valeurConnue = false;
@@ -43,53 +54,53 @@ public class Carte {
     }
 
     /**
-     * @return couleur	La couleur de la carte
+     * @return couleur La couleur de la carte
      */
-    public String getCouleur() {
-	return couleur;
+    public Couleur getCouleur() {
+	return this.couleur;
     }
 
     /**
-     * @return valeur	La valeur de la carte
+     * @return valeur La valeur de la carte
      */
     public int getValeur() {
-	return valeur;
+	return this.valeur;
     }
 
     /**
-     * @return couleurConnue	Le status de la visibilité de la couleur 
-     * 			        de la carte
+     * @return couleurConnue Le status de la visibilité de la couleur 
+     *         de la carte
      */
     public boolean getCouleurConnue() {
-	return couleurConnue;
+	return this.couleurConnue;
     }
 
     /**
-     * @return valeurConnue  Le status de la visibilité de la valeur
-     * 			     de la carte
+     * @return valeurConnue Le status de la visibilité de la valeur
+     * 	       de la carte
      */
     public boolean getValeurConnue() {
-	return valeurConnue;
+	return this.valeurConnue;
     }
 
     /**
      * Défini si la couleur est visible ou non.
      * 
-     * @param etat	État de la visibilité de la couleur. True pour couleur
-     *  		connue sinon false
+     * @param etat État de la visibilité de la couleur. True pour couleur
+     *        connue sinon false
      */
     public void setCouleurConnue(boolean etat) {
-	couleurConnue = etat;
+        this.couleurConnue = etat;
     }
 
     /**
      * Défini si la valeur est visible ou non.
      *
-     * @param etat	État de la visibilité de la valeur. True pour valeur
-     * 			connue sinon false
+     * @param etat État de la visibilité de la valeur. True pour valeur
+     *        connue sinon false
      */
     public void setValeurConnue(boolean etat) {
-	valeurConnue = etat;
+	this.valeurConnue = etat;
     }
 
     /**
@@ -97,7 +108,7 @@ public class Carte {
      * visibilité de ces deux champs.
      * 
      * @return Une chaîne de caractères représentant la carte au format 
-     * [Couleur Valeur].
+     *         [Couleur Valeur couleurConnue valeurConnue].
      */
     @Override
     public String toString() {
