7c3c6c2080e2280456daf18af28a374f528cfbf4
aurelien.valat <aurelien.valat@host.docker.internal>
2024-05-31 11:20:49 +0200

    bb

---
+++ src/main/jeu/application/controleur/ChangerTheme.java
@@ -0,0 +1,12 @@
+/*
+ * ChangerTheme.java                                            31 mai 2024
+ * IUT de Rodez, Info1 2023-2024 TP4, pas de copyright
+ */
+package jeu.application.controleur;
+
+/**
+ * TODO commenter la responsabilité de cette classe (SRP)
+ */
+public class ChangerTheme {
+
+}
